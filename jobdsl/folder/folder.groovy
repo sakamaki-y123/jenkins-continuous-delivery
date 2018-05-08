ee3fb4c83298a215dee4107d390a461162a8bbcc
Yuto Sakamaki <ysaka123@gmail.com>
2018-05-08 22:13:00 +0900

    refs #0 fix folder.groovy

--- jobdsl/folder/folder.groovy
+++ jobdsl/folder/folder.groovy
@@ -1,1 +1,16 @@
-folder("${FOLDER_PATH}")
+def createFolder(String folderPath) {
+    folderPathString = "";
+    folderPath.split("/").each { path ->
+        if (!folderPathString.isEmpty()) {
+            folderPathString = "${folderPathString}/"
+        }
+        folderPathString = "${folderPathString}${path}"
+        folder("${folderPathString}"){
+            description(
+				'repository: https://github.com/sakamaki-y123/jenkins-continuous-delivery' 
+			)
+        }
+    }
+}
+
+createFolder("${FOLDER_PATH}")
