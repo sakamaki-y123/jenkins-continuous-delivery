5a554e9f5ae011557af264640ef130be702cb2bd
Yuto Sakamaki <ysaka123@gmail.com>
2018-05-12 16:07:10 +0900

    refs #0 fix bug seed job dsl

--- pipeline/decrative-pipeline/pipeline_00_seed_pipeline.groovy
+++ pipeline/decrative-pipeline/pipeline_00_seed_pipeline.groovy
@@ -15,7 +15,7 @@ pipeline {
                         'jobdsl/pipeline/pipeline_01_hello_world.groovy',
                         'jobdsl/pipeline/pipeline_02_use_sh_step.groovy',
                         'jobdsl/pipeline/pipeline_03_write_file.groovy',
-                        'jobdsl/pipeline/pipeline_04_archive_artifacts'
+                        'jobdsl/pipeline/pipeline_04_archive_artifacts.groovy'
                     ].join('\n'),
                     additionalParameters: [
                         FOLDER_PATH: "${FOLDER_PATH}"
