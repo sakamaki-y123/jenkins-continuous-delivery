cf11357f5dc4a27f597d607f84ea068d01c03523
Yuto Sakamaki <ysaka123@gmail.com>
2018-05-18 00:57:25 +0900

    refs #0 fix pipeline

--- pipeline/decrative-pipeline/pipeline_13_conditional_run_stage.groovy
+++ pipeline/decrative-pipeline/pipeline_13_conditional_run_stage.groovy
@@ -1,89 +1,45 @@
 /**
  * TOPICS
- * - timeout
- *     - https://jenkins.io/doc/pipeline/steps/workflow-basic-steps/#code-timeout-code-enforce-time-limit
- * - input
- *     - https://jenkins.io/doc/book/pipeline/syntax/#input
- *     - https://jenkins.io/doc/pipeline/steps/pipeline-input-step/#code-input-code-wait-for-interactive-input
+ * - when
+ *     - https://jenkins.io/doc/book/pipeline/syntax/#when
  */
 
-def SAMPLE_YAML = """\
-name:
-  first: ""
-  last: ""
-dates:
-  birth: ""
-"""
-
-def datas
+def boolean skipStage( String startStageNo, String stageNo ){
+    if( startStageNo.toInteger() <= stageNo.toInteger() ){
+        return false
+    } else {
+        return true
+    }
+}
 
 pipeline {
     agent any
 
-    environment {
-        fileName = "sample.yml"
-    }
-
     stages {
-        stage('read sample yaml') {
+        stage('stage1') {
+
+            when {
+                environment name: 'START_STAGE_NO', value: '1'
+            }
+
             steps {
-                script{
-                    datas = readYaml text: "${SAMPLE_YAML}"
-                }
+                echo "run stage 1"
             }
         }
 
-        stage('input name') {
-            steps {
-                script{
-                    timeout(time:3, unit:'MINUTES') {
-                        inputName = input(
-                            id: "inputName",
-                            message: "Please input your name.",
-                            parameters:[
-                                string( name: 'first', defaultValue: '', description: 'Input your first name.'),
-                                string( name: 'last', defaultValue: '', description: 'Input your last name.')
-                            ]
-                        )
-                        datas.name.first = inputName.first
-                        datas.name.last = inputName.last
+        stage('stage2') {
+
+            when{
+                not{
+                    expression {
+                        return skipStage( START_STAGE_NO, "2" )
                     }
                 }
             }
-        }
-
-        stage('input birthday') {
+            
             steps {
-                script{
-                    timeout(time:3, unit:'MINUTES') {
-                        inputBirthday = input(
-                            id: "inputBirthday",
-                            message: "Please input your birth day.",
-                            parameters:[
-                                string( name: 'birth', defaultValue: 'yyyy-MM-dd', description: ''),
-                            ]
-                        )
-                        datas.dates.birth = inputBirthday
-                    }
-                }
+                echo "run stage 2"
             }
-        }
-
-        stage('write yaml') {
-            steps {
-                script{
-                    echo "Name is ${datas.name.first} ${datas.name.last}"
-                    echo "Birthday is ${datas.dates.birth}"
-                    writeYaml file: fileName, data: datas
-                    archiveArtifacts fileName
-                }
-            }
-        }
-    }
-
-    post {
-        always {
-            cleanWs()
         }
     }
 }
