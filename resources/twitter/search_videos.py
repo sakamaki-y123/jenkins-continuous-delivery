2bee3daff09d827764aa0ea4d1bfe79da3043dab
Yuto Sakamaki <ysaka123@gmail.com>
2018-10-27 18:42:36 +0900

    fix search_videos.py

--- resources/twitter/search_videos.py
+++ resources/twitter/search_videos.py
@@ -24,22 +24,25 @@ def get_video_url(status,video_infos):
         for media in status.extended_entities.get('media', [{}]):
             if media.get('type', None) == 'video' :
                 for variants in media['video_info']['variants']:
-                    print(variants)
                     if (variants.get('bitrate', None) == 2176000) and (variants['content_type'] == 'video/mp4'):
+                        print(media)
                         video_infos.append(variants['url'])
     return list(set(video_infos))
 
 def main(args):
     keyword = args.keyword
     search_result_type = args.result_type
-    search_count = args.count
+    search_count = int(args.count)
     max_result = int(args.max_result)
     result_json = args.file
     video_infos = []
-    for status in tweepy.Cursor(api.search,q=keyword,count = search_count,result_type = search_result_type).items():
+    for status in tweepy.Cursor(api.search,q=keyword,result_type = search_result_type,count = search_count).items():
         video_infos = get_video_url(status, video_infos)
         if(max_result <= len(video_infos)):
             break;
+
+    print('--- result ---')
+    print(video_infos)
 
     fw = open(result_json, 'w')
     json.dump(video_infos, fw, indent=4)
@@ -52,7 +55,7 @@ if __name__ == '__main__':
     parser.add_argument('-k', '--keyword', nargs='+', required=True)
     parser.add_argument('-f', '--file', required=False ,default="search_video_result.json")
     parser.add_argument('-r', '--result-type', required=False, default="recent")
-    parser.add_argument('-c', '--count', required=False, default="500")
+    parser.add_argument('-c', '--count', required=False, default="15000")
     parser.add_argument('-m', '--max-result', required=False, default="30")
 
     args = parser.parse_args()
